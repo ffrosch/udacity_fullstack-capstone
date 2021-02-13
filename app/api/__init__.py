701b72fb60f764fd3150d99e8727683dee08008f
Florian Frosch <florian.frosch@posteo.de>
2021-02-13 12:56:08 +0100

    feat: add TourAPI and user_status decorator

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -6,3 +6,11 @@ api = Blueprint('api', __name__)
 test_view = TestAPI.as_view('api')
 api.add_url_rule('/', view_func=test_view)
 
+tour_view = TourAPI.as_view('tours')
+api.add_url_rule('/tours/',
+                 defaults={'tour_id': None},
+                 view_func=tour_view,
+                 methods=['GET'])
+api.add_url_rule('/tours/<int:tour_id>',
+                 view_func=tour_view,
+                 methods=['GET', 'PATCH', 'DELETE'])
--- app/api/routes.py
+++ app/api/routes.py
@@ -15,6 +15,29 @@ def test(arg=''):
     return test_decorator
 
 
+def user_status(f):
+    def decorator(*args, **kwargs):
+        user = {}
+        token = None  # TODO: implement get_token_auth_header
+        payload = None  # TODO: implement get_payload
+        auth = False  # TODO: implement check_auth() -> True/False
+        if auth:
+            user['auth'] = True
+            user['permissions'] = payload['permissions']
+            user['id'] = payload['user_id']
+        else:
+            user['auth'] = False
+            user['permissions'] = []
+            user['id'] = None
+        return f(*args, user=user, **kwargs)
+    return decorator
+
+
+def get_permissions():
+    # TODO: Implement
+    pass
+
+
 class TestAPI(MethodView):
     decorators = [test('Tested')]
 
@@ -23,3 +46,45 @@ class TestAPI(MethodView):
 
     def post(self):
         return jsonify(success=True, message='Hello POST')
+
+
+class TourAPI(MethodView):
+    decorators = [user_status]
+
+    def get(self, tour_id, user):
+        # GET All
+        if tour_id is None:
+            if user['auth']:
+                if 'admin' in user['permissions']:
+                    tours = Tour.query.all()
+                else:
+                    tours = Tour.query.filter(or_(
+                        Tour.accesslevel.has(name='Public'),
+                        Tour.user_id == user['id'])
+                    ).all()
+            else:
+                tours = Tour.query.filter(
+                    Tour.accesslevel.has(name='Public')
+                ).all()
+        # GET <int:tour_id>
+        else:
+            tour = Tour.query.filter(Tour.id == tour_id).first()
+            if tour is None:
+                abort(404)
+            if tour.accesslevel.name == 'Private':
+                if not user['auth']:
+                    abort(401)
+                if tour.user_id != user['id']:
+                    abort(403)
+            tours = [tour]
+
+        data = {'success': True,
+                'tours': [tour.as_geojson() for tour in tours]}
+
+        return jsonify(data), 200
+
+    def patch(self, tour_id, user):
+        pass
+
+    def delete(self, tour_id, user):
+        pass
