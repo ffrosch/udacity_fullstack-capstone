0064a42afecacf708c610f70b8e386e259b8cc8e
Florian Frosch <florian.frosch@posteo.de>
2021-02-14 02:07:33 +0100

    refactor: move errorhandlers into errors.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, jsonify
-from .auth import AuthError
+from flask import Blueprint
 from .routes import APIHome, ActivityAPIModerator, ActivityAPIPublic, TourAPI
 
 api = Blueprint('api', __name__)
+
+from . import errors
 
 home_view = APIHome.as_view('home')
 api.add_url_rule('/', view_func=home_view)
@@ -32,60 +33,3 @@ api.add_url_rule('/activities/<int:activity_id>',
 api.add_url_rule('/activities/<int:activity_id>',
                  view_func=activity_view_moderator,
                  methods=['PATCH', 'DELETE'])
-
-
-######################
-### Error Handlers ###
-######################
-
-# Errors 404 and 500 are handled by the main flask app
-# They cannot reliably be handled by blueprints
-# See flask.blueprint docs for further information
-
-@api.errorhandler(400)
-def _handle_400_error(error):
-    return jsonify({
-        "success": False,
-        "error": 400,
-        "message": "bad request"
-    }), 400
-
-@api.errorhandler(401)
-def _handle_401_error(error):
-    return jsonify({
-        "success": False,
-        "error": 401,
-        "message": "unauthorized"
-    }), 401
-
-@api.errorhandler(403)
-def _handle_403_error(error):
-    return jsonify({
-        "success": False,
-        "error": 403,
-        "message": "forbidden"
-    }), 403
-
-@api.errorhandler(405)
-def _handle_405_error(error):
-    return jsonify({
-                    "success": False,
-                    "error": 405,
-                    "message": "not allowed"
-                }), 405
-
-@api.errorhandler(422)
-def _handle_422_error(error):
-    return jsonify({
-        "success": False,
-        "error": 422,
-        "message": "unprocessable"
-    }), 422
-
-@api.errorhandler(AuthError)
-def auth_error(error):
-    return jsonify({
-        "success": False,
-        "error": error.status_code,
-        "message": error.error['description']
-    }), error.status_code
---
+++ app/api/errors.py
@@ -0,0 +1,60 @@
+from flask import jsonify
+
+from . import api
+from .auth import AuthError
+
+######################
+### Error Handlers ###
+######################
+
+# Errors 404 and 500 are handled by the main flask app
+# They cannot reliably be handled by blueprints
+# See flask.blueprint docs for further information
+
+@api.errorhandler(400)
+def _handle_400_error(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "bad request"
+    }), 400
+
+@api.errorhandler(401)
+def _handle_401_error(error):
+    return jsonify({
+        "success": False,
+        "error": 401,
+        "message": "unauthorized"
+    }), 401
+
+@api.errorhandler(403)
+def _handle_403_error(error):
+    return jsonify({
+        "success": False,
+        "error": 403,
+        "message": "forbidden"
+    }), 403
+
+@api.errorhandler(405)
+def _handle_405_error(error):
+    return jsonify({
+                    "success": False,
+                    "error": 405,
+                    "message": "not allowed"
+                }), 405
+
+@api.errorhandler(422)
+def _handle_422_error(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
+
+@api.errorhandler(AuthError)
+def auth_error(error):
+    return jsonify({
+        "success": False,
+        "error": error.status_code,
+        "message": error.error['description']
+    }), error.status_code
